c47fb068419b8f2e6ed56eefcf376faca1f07607
Cathella <ncathie214@gmail.com>
2021-12-14 15:29:34 +0300

    Implement add book in Form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,36 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { bindActionCreators } from 'redux';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
-const dispatch = useDispatch();
-
-const submitBookToStore = () => {
-  const newBook = {
-    id, title, author,
-  }
-
-  dispatch(addBook(newBook));
-}
-
 const categories = ['True Crime', 'Horror', 'Action & Adventure', 'Comic', 'Detective & Mystery', 'Fantasy', 'Historical Fiction', 'Poetry', 'Romance', 'Short Stories', 'Suspense & Thrillers', 'Biographies', 'Cookbooks', 'Essays', 'Memoirs'];
 
-const Form = () => (
-  <div className="form">
-    <form>
-      <h3>Add New Book</h3>
-      <section>
-        <input name="title" placeholder="Book title" />
-        <select name="category">
-          {
-            categories.map((category) => (
-              <option key={category} value={category}>{category}</option>
-            ))
-          }
-        </select>
-        <button onClick={submitBookToStore} type="button">Add Book</button>
-      </section>
-    </form>
-  </div>
-);
+const Form = () => {
+  const dispatch = useDispatch();
+  const createBookAction = bindActionCreators(addBook, dispatch);
+
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+
+    const id = uuidv4();
+
+    if (title && category && author) {
+      createBookAction({
+        title, author, category, id,
+      });
+      setTitle('');
+      setCategory('');
+      setAuthor('');
+    }
+  };
+
+  return (
+    <div className="form">
+      <form onSubmit={submitBookToStore}>
+        <h3>Add New Book</h3>
+        <section>
+          <input id="title" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} type="text" value={title} />
+          <select id="category" onChange={(e) => setCategory(e.target.value)} value={category}>
+            {
+              categories.map((category) => (
+                <option key={category} value={category}>{category}</option>
+              ))
+            }
+          </select>
+          <input id="author" placeholder="Book Author" onChange={(e) => setAuthor(e.target.value)} type="text" value={author} />
+          <button type="submit">Add Book</button>
+        </section>
+      </form>
+    </div>
+  )
+};
 
 export default Form;
