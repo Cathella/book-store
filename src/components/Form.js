12bd4403e0aec87236789089f735ff7d44256789
Cathella <ncathie214@gmail.com>
2021-12-14 06:35:02 +0300

    Populate book categories from an array

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const categories = ['True Crime', 'Horror', 'Action & Adventure', 'Comic', 'Detective & Mystery', 'Fantasy', 'Historical Fiction', 'Poetry', 'Romance', 'Short Stories', 'Suspense & Thrillers', 'Biographies', 'Cookbooks', 'Essays', 'Memoirs'];
+
 const Form = () => {
   return (
     <div>
@@ -9,7 +11,11 @@ const Form = () => {
         </label>
         <input name="title" placeholder="Book title" />
         <select name="category">
-          <option>Category</option>
+          {
+            categories.map((category) => (
+              <option key={category} value={category}>{category}</option>
+            ))
+          }
         </select>
         <button type="button">Add Book</button>
       </form>
