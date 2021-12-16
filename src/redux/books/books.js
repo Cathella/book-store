8dd85cb385845678391f2204c0d7aea27e02aea9
Cathella <ncathie214@gmail.com>
2021-12-16 10:57:23 +0300

    Add function to list books from Api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,8 +1,8 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookStore/books/GET_BOOKS';
 
 const initialState = [];
-const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/:app_id/books';
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -14,7 +14,13 @@ export const removeBook = (payload) => ({
   payload,
 });
 
+export const getBooks = (payload) => ({
+  type: GET_BOOKS,
+  payload,
+})
+
 export const addBookToApi = (payload) => async (dispatch) => {
+  const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/:app_id/books';
   const response = await fetch(url, {
     method: 'POST',
     body: JSON.stringify(payload),
@@ -29,6 +35,7 @@ export const addBookToApi = (payload) => async (dispatch) => {
 };
 
 export const removeBookFromApi = (payload) => async (dispatch) => {
+  const url = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/:app_id/books/${payload}`;
   const response = await fetch(url, {
     method: 'DELETE',
   });
@@ -38,8 +45,22 @@ export const removeBookFromApi = (payload) => async (dispatch) => {
   }
 };
 
+export const listBooks = async () => {
+  const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/:app_id/books';
+  const response = await fetch(url);
+  const books = await response.json();
+  const newBooks = Object.keys(books).map((key) => ({
+    ...books[key][0],
+    item_id: key,
+  }))
+
+  return Object.values(newBooks);
+} 
+
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_BOOKS:
+      return action.payload;
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
